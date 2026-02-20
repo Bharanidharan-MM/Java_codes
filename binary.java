a8854a3e327d501917350282052b5a22dd4740b7
Bharanidharan-MM <bharanidharanmaris@gmail.com>
2026-02-20 21:15:47 +0530

    Add files via upload

---
+++ binary.java
@@ -0,0 +1,14 @@
+class binary{
+    public static void main(String[]args){
+
+        int a = 8;
+        String res = " ";
+        while(a>0){
+            int rem = a%2;
+            res = rem+res;
+            a = a/2;
+
+        }
+        System.out.println(res);
+    }
+}
