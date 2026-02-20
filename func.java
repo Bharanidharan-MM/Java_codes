81d2766f6f1e22bcc1effef978b48e6093beb7a3
Bharanidharan-MM <bharanidharanmaris@gmail.com>
2026-02-20 21:21:29 +0530

    Add files via upload

---
+++ func.java
@@ -0,0 +1,16 @@
+public class func{
+    void greeding(){
+
+        System.out.println("hey guys......");
+    }
+
+    void demo(){
+        System.out.println("welcome all");
+    }
+    public static void main(String[] args) {
+        
+        func a = new func();
+        a.greeding();
+        a.demo();
+    }
+}
